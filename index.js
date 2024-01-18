de2796475912feb6128e7ba87a90e896a4e9d442
Noah <noahdevelopsthings@gmail.com>
2024-01-17 23:43:07 -0400

    LOC?

--- index.js
+++ index.js
@@ -23,4 +23,34 @@ axios.get('https://streak-stats.demolab.com?user=noahrepublic').then((response) 
     }
 });
 
+const promises = [];
 
+let linesWrote = 0;
+
+axios.get("https://api.github.com/users/noahrepublic/repos").then((response) => {
+    const repos = response.data;
+    let totalStars = 0;
+
+    const promise = repos.forEach((repo) => {
+      
+        axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then((response) => {
+            console.log(response.data[-1].linesOfCode)
+
+            if (typeof response.data[-1].linesOfCode === 'number') {
+                linesWrote += parseInt(response.data[-1].linesOfCode);
+            }
+        });
+    });
+
+    promises.push(promise);
+});
+
+Promise.all(promises).then(() => {
+    if (typeof linesWrote === 'number') {
+        fs.writeFile("linesWrote.txt", linesWrote.toString(), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+});
