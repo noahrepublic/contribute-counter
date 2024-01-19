adc7dbcc57928f5796bd1c1e7da4537a15882f1d
Noah <noahdevelopsthings@gmail.com>
2024-01-19 00:24:45 -0400

    Lines of code fixed?

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const axios = require('axios');
 
 const fs = require("fs");
 
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
 
+function promiseSleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
 // we need to also use .get on each takes owner, and repo
 
 let totalContributes = 0;
@@ -29,28 +40,44 @@ let linesWrote = 0;
 
 axios.get("https://api.github.com/users/noahrepublic/repos").then((response) => {
     const repos = response.data;
-    let totalStars = 0;
+ 
+    repos.forEach((repo) => {
+        function request(response) {
+            const length = response.data.length;
+            console.log(response.data)
+            console.log(response.data[length - 1])
 
-    const promise = repos.forEach((repo) => {
-      
-        axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then((response) => {
-            console.log(response.data[-1].linesOfCode)
-
-            if (typeof response.data[-1].linesOfCode === 'number') {
-                linesWrote += parseInt(response.data[-1].linesOfCode);
+            if (typeof response.data[length - 1].linesOfCode === 'number') {
+                linesWrote += parseInt(response.data[length - 1].linesOfCode);
             }
-        });
+
+            if (typeof linesWrote === 'number') {
+                fs.writeFile("linesWrote.txt", linesWrote.toString(), (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                });
+            }
+
+            console.log(linesWrote)
+        }
+
+        function onFail() {
+            console.log("Failed to get " + repo.full_name)
+
+            promises.push(promiseSleep(60*1000).then(() => {
+                console.log("Retrying " + repo.full_name)
+                const retryPromise = axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then(request).catch(onFail);
+
+                promises.push(retryPromise);
+            }))
+        }
+
+        console.log("Requesting" + repo.full_name)
+        const promise =  axios.get(`https://api.codetabs.com/v1/loc?github=${repo.full_name}`).then(request).catch(onFail);
+
+        promises.push(promise);
+        sleep(10 * 1000)
     });
-
-    promises.push(promise);
 });
 
-Promise.all(promises).then(() => {
-    if (typeof linesWrote === 'number') {
-        fs.writeFile("linesWrote.txt", linesWrote.toString(), (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-});
